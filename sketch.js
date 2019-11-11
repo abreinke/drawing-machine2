66b56a45c95e59e7f605b1abeed7a9a0a8432f9b
abreinke <42782067+abreinke@users.noreply.github.com>
2019-11-10 17:59:52 -0800

    changes

--- sketch.js
+++ sketch.js
@@ -1,10 +1,20 @@
 function setup() {
   createCanvas(1000, 1000);
   background(255, 255, 255);
-}
+  strokeWeight(50);
+  }
 
 function draw() {
-  if (mouseIsPressed == true) {
-     ellipse(mouseX, mouseY, 75, 75);
-   } 
+if (mousePressed == true){
+line(width - mouseX, height - mouseY, width - pmouseX, height - pmouseY);
+line(mouseX, mouseY, pmouseX, pmouseY);
+}
+}
+function draw() {
+  if (keyIsPressed === true) {
+    fill(0);
+  } else {
+    fill(255);
+  }
+line(25, 25, 50, 50);
 }

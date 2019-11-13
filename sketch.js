4ef1775d03543281e980be04c0069a859c103d6b
abreinke <42782067+abreinke@users.noreply.github.com>
2019-11-12 17:23:07 -0800

    changes

--- sketch.js
+++ sketch.js
@@ -1,20 +1,42 @@
 function setup() {
-  createCanvas(1000, 1000);
-  background(255, 255, 255);
-  strokeWeight(50);
-  }
+createCanvas(500, 500);
+background(252, 251, 227);
+strokeWeight(25);
+}
 
 function draw() {
-if (mousePressed == true){
+if (mouseIsPressed){
 line(width - mouseX, height - mouseY, width - pmouseX, height - pmouseY);
 line(mouseX, mouseY, pmouseX, pmouseY);
-}
-}
-function draw() {
-  if (keyIsPressed === true) {
-    fill(0);
-  } else {
-    fill(255);
   }
-line(25, 25, 50, 50);
+}
+
+function keyTyped(){
+  if (key === 'r'){
+  stroke(255, 64, 75);
+  }
+  else if(key === 'g') {
+  stroke(204, 255, 0);
+  }
+  return false;
+}
+
+function keyTyped() {
+  if (key === 'y') {
+  stroke(255, 245, 0);
+  }
+  else if (key === 'p') {
+  stroke(190, 128, 255);
+  }
+  return false;
+}
+
+function keyTyped(){
+if (key === 'b'){
+stroke(186, 228, 229);
+  }
+  else if ( key === 'o') {
+  stroke(251, 184, 41);
+  }
+  return false;
 }
